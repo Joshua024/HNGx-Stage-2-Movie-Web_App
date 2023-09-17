b8f2401f5a46209b5ab6fa5014bb43559e1fa4a8
Joshua024 <adumchimajoshua@gmail.com>
2023-09-17 20:41:54 +0100

    Fix case-sensitivity issue with Card.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import MoviePage from './pages/MoviePage';
 
 function App() {
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const storedMovie = localStorage.getItem("Movie");
+
+    try {
+      const parsedMovie = JSON.parse(storedMovie);
+      setMovie(parsedMovie);
+    } catch (error) {
+      console.error("Error parsing movie data:", error);
+    }
+  }, []);
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>
     <Router>
       <Routes>
-        <Route path='/' element={<Homepage/>}/>
-        <Route path='/single-movie' element={<MoviePage/>}/>
+        <Route path='/' element={<Homepage />} />
+        {/* Pass the movie data as a prop to the MoviePage component */}
+        <Route path='/movies/:movie_id' element={<MoviePage movie={movie} />} />
       </Routes>
     </Router>
-  
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
